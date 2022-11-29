69fa9585082bcd3b2f3f7a52acb945a72d834857
worjs320 <worjs320@naver.com>
2022-11-30 01:03:52 +0900

    학습 노트 관련 DB 스키마 추가

--- content/admin.py
+++ content/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Content
+from .models import Note
 
 admin.site.register(Content)
+admin.site.register(Note)
--- content/models.py
+++ content/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Content(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     user_name = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200)
     youtube_id = models.CharField(max_length=200, default='', null=True)
@@ -14,9 +14,24 @@ class Content(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+class Note(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user_name = models.CharField(max_length=200, null=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.content.title + '에 대한 댓글'
